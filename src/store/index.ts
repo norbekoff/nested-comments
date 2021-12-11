632f3aa31b0acea39dc5af802bb18d76e59c7dd9
unicorno <jasurbeknorbekov@gmail.com>
2021-12-11 22:22:20 +0500

    refactor: store idndex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,28 @@
 import { createStore } from "vuex";
 
 export default createStore({
-    state: {
-        comments: [
-            {
-                title: 'First comment',
-                id: 1,
-                author: 'John Doe',
-                authorPic: 'https://randomuser.me/api/portraits/',
-                date: '2020-01-01',
-                reply: [
-                    {
-                        title: 'Second comment',
-                        id: 2,
-                        author: 'John Doe',
-                        authorPic: 'https://randomuser.me/api/portraits/',
-                        date: '2020-01-01',
-                        reply: []
-                    }
-                ]
-            }
-        ]
-    },
-    mutations: {},
-    actions: {},
-    modules: {},
+  state: {
+    comments: [
+      {
+        title: "First comment",
+        id: 1,
+        author: "John Doe",
+        authorPic: "https://randomuser.me/api/portraits/",
+        date: "2020-01-01",
+        reply: [
+          {
+            title: "Second comment",
+            id: 2,
+            author: "John Doe",
+            authorPic: "https://randomuser.me/api/portraits/",
+            date: "2020-01-01",
+            reply: [],
+          },
+        ],
+      },
+    ],
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
 });
