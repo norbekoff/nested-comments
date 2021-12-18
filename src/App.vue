d0927f573fd8e137955361e43d9a568c08df304a
norbekoff <jasurbeknorbekov@gmail.com>
2021-12-18 10:09:40 +0500

    feat: add comment component and git ignore

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-
 <template>
   <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          Hello
-      </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
+    <router-view />
+    <Comment/>
   </div>
 </template>
+
+<script>
+import Comment from '@/components/Comment.vue'
+export default {
+  components: {
+    Comment
+  }
+}
+</script>
--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,11 +1,69 @@
 <template>
   <div class="comment">
-      comment
+    <div class="comment__avatar">
+      <img :src="avatar" alt="" />
+    </div>
+    <div class="comment__content">
+      <h3 class="comment__title">
+        {{ author }}
+      </h3>
+      <p class="comment__body">
+        {{ comment }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    avatar: {
+      type: String,
+      default: "https://picsum.photos/200",
+    },
+    author: {
+      type: String,
+      default: "Robin Wieruch",
+    },
+    comment: {
+      type: String,
+      default: "What is all this then?",
+    },
+    reply: []
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.comment {
+  display: flex;
+  gap: 8px;
+  // .comment__avatar
+  &__avatar {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  // .comment__content
+
+  &__content {
+  }
+
+  // .comment__title
+
+  &__title {
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 16px;
+  }
+
+  // .comment__body
+
+  &__body {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+  }
+}
+</style>
